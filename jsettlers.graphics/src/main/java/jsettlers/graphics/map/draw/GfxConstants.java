9b1709500882389201b6f20aa3a5e4c8c22f5f83
Jokuhl <Jokuhlblade@gmail.com>
2021-01-23 21:05:03 +0100

    added egyptian slaughterhouse, mines, ironmelt, goldmelt, toolsmith and weaponsmith to fully build and function like the original ones. Adjusted some of the GfxConstants.

--- jsettlers.graphics/src/main/java/jsettlers/graphics/map/draw/GfxConstants.java
+++ jsettlers.graphics/src/main/java/jsettlers/graphics/map/draw/GfxConstants.java
@@ -46,16 +46,16 @@ public final class GfxConstants {
 
     //use the BuildingCreatorApp in order to find the tile where you want to draw relatively to the building's position
     //TODO: the tile system is a huge problem for placing animations correctly, is there a way to draw meticulous?
-    public static final int[][] XY_OFFSET_SMOKE_IRONMELT = {{-7, -9},{-5, -6},{-6, -8},{-6, -9}};
+    public static final int[][] XY_OFFSET_SMOKE_IRONMELT = {{-7, -9},{-4, -5},{-6, -8},{-6, -9}};
     public static final int[][] XY_OFFSET_METAL_IRONMELT = {{0, 0},{0, 0},{0, 0},{0, 0}};
-    public static final int[][] XY_OFFSET_SMOKE_GOLDMELT = {{-7, -10},{-5, -6},{-5, -5},{-7, -10}};
+    public static final int[][] XY_OFFSET_SMOKE_GOLDMELT = {{-7, -10},{-4, -5},{-5, -5},{-7, -10}};
     public static final int[][] XY_OFFSET_METAL_GOLDMELT = {{0, 0},{0, 0},{0, 0},{0, 0}};
 
     public static final int[][] XY_OFFSET_SMOKE_BAKER = {{-3, -5},{1, -2},{-2, -3},{-1, -7}};
 
     public static final int[] XY_OFFSET_SMOKE_CHARCOALBURNER = {-4, -6};
 
-    public static final int[][] XY_OFFSET_SMOKESMITH_WEAPONSMITH = {{-3, -2},{-3, -2},{-3, -2},{-3, -2}};
+    public static final int[][] XY_OFFSET_SMOKESMITH_WEAPONSMITH = {{-3, -2},{-2, 0},{-3, -2},{-3, -2}};
     public static final int[][] XY_OFFSET_SMOKESMITH_TOOLSMITH = {{-3, -2},{-2, -1},{-2, -1},{-3, -2}};
 
     /**
